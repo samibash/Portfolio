f23aed90f92f4b458e52203d0ed6000746ed2a12
Sami Bachir <samibachir20@email.com>
2019-06-13 17:37:49 -0400

    installed react burger menu, styled nav

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
+import { bubble as Menu } from 'react-burger-menu';
 import Home from '../Home/Home';
 import Works from '../Works/Works';
 import Contact from '../Contact/Contact';
@@ -9,20 +10,11 @@ export default class Navigation extends Component {
     render() {
         return (
             <div>
-                <aside class="menu">
-                    <p class="menu-label">
-                        Sami Bachir
-                    </p>
-                    <ul class="menu-list">
-                        <Link className="navbar-item" to={'/'}><p>Home</p></Link>
-                        <Link className="navbar-item" to={'/works'}><p>Works</p></Link>
-                        <Link className="navbar-item" to={'/contact'}><p>Contact</p></Link>
-                    </ul>
-                </aside>
-
-
-
-
+                <Menu>
+                    <Link className="menu-item" to={'/'}><p>Home</p></Link>
+                    <Link className="menu-item" to={'/works'}><p>Works</p></Link>
+                    <Link className="menu-item" to={'/contact'}><p>Contact</p></Link>
+                </Menu>
 
 
                 <Switch>

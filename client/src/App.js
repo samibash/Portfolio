001143a2a5d8a677aba667dfeb8db6e22a20be12
Sami Bachir <samibachir20@email.com>
2019-06-13 14:29:27 -0400

    things i did: installed react router dom, created all components, housecleaned, removed unnecessary things

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Contact from './components/Contact/Contact';
+import Home from './components/Home/Home';
+import Navigation from './components/Navigation/Navigation';
+import Works from './components/Works/Works';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
+    </div>
+  );
+}
+
+export default App;

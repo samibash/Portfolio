21789aa1bd8c9ad940621a0801fb306161af41f4
Sami Bachir <samibachir20@email.com>
2019-06-17 21:39:07 -0400

    finished editing and styling contact page

--- client/src/components/Contact/Contact.js
+++ client/src/components/Contact/Contact.js
@@ -1,33 +1,51 @@
 import React, {Component} from 'react';
+import './Contact.css'
 
 export default class Contact extends Component {
     render() {
         return (
             <div>
-                <h1>
-                    Get in touch
-                </h1>
-                <p>
-                    If you want to get in touch and talk about a project collaboration or to 
-                    just say hello, you can send me an email at <a href="mailto:samibachir20@gmail.com">samibachir20@gmail.com</a>
-                </p>
+                <section class="section is-medium">
+                    <div class="container">
+                        <h1 class="title hvr-buzz-out" id="contact-title">Get in touch</h1>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <div class="container is-fluid">
+                            <div>
+                                <h2 class="subtitle" id="contact-subtitle">
+                                If you want to get in touch and talk about a project collaboration or to 
+                                just say hello, you can send me an email at 
+                                <a href="mailto:samibachir20@gmail.com"> samibachir20@gmail.com</a>. Or, connect
+                                with me below:</h2>
+                            </div>
+                        </div>
+                    </div>
+                </section>   
+                <br/>
+                <br/>
+                <br/>
+ 
                 <footer>
-                    <h1>
-                        ~Socialize~
-                    </h1>
-
-                    <div class="columns">
-                        <div class="column">
-                            <a class="button is-link">Facebook</a>  
-                        </div>
-                        <div class="column">
-                            <a class="button is-info">LinkedIn</a>
-                        </div>
-                        <div class="column">
-                            <a class="button is-dark">Github</a>
-                        </div>
-                        <div class="column">
-                            <a class="button is-danger">Instagram</a>
+                    <div class="content has-text-centered">
+                        <div class="columns">
+                            <div class="column">
+                                <a class="button is-medium is-link is-rounded hvr-grow"><i class="fab fa-facebook-square"></i>Facebook</a>  
+                            </div>
+                            <div class="column">
+                                <a class="button is-medium is-info is-rounded hvr-grow"><i class="fab fa-linkedin"></i>LinkedIn</a>
+                            </div>
+                            <div class="column">
+                                <a class="button is-medium is-dark is-rounded hvr-grow"><i class="fab fa-github-square"></i>Github</a>
+                            </div>
+                            <div class="column">
+                                <a class="button is-medium is-danger is-rounded hvr-grow"><i class="fab fa-instagram"></i>Instagram</a>
+                            </div>
                         </div>
                     </div>
                 </footer>
@@ -35,3 +53,6 @@ export default class Contact extends Component {
         )
     }
 }
+
+
+   
